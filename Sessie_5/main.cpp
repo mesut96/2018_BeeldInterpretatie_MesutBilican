d4c620334c1ca053d06a2284312f2410843aeaf0
mesut96 <mesut.bilican@hotmail.com>
2018-12-04 21:24:21 +0100

    crashes after bayes training

--- Sessie_5/main.cpp
+++ Sessie_5/main.cpp
@@ -6,14 +6,12 @@
 #include "opencv2/highgui.hpp"
 #include <opencv2/features2d/features2d.hpp>
 
+
 using namespace std;
 using namespace cv;
+using namespace ml;
 
 /// Function Headers
-void ORB_KeypointsDetection(Mat tmpl, Mat img);
-void BRISK_KeypointsDetection(Mat tmpl, Mat img);
-void AKAZE_KeypointsDetection(Mat tmpl, Mat img);
-void KeypointMatching(string name, Mat tmpl, Mat img, std::vector<KeyPoint> keypoints_tmpl, std::vector<KeyPoint> keypoints_img, Mat descriptor_tmpl, Mat descriptor_img);
 static void onMouseStrawberries( int event, int x, int y, int, void* );
 static void onMouseBackground( int event, int x, int y, int, void* );
 
@@ -61,18 +59,23 @@ int main( int argc, char** argv )
     //imshow("strawberry2", img_strawberry2);
     //waitKey(0);
 
-    /// Gaussian blur images (5,5)
+    /// Gaussian blur
+    Mat gaussBlur;
+
+    GaussianBlur(img_strawberry1, gaussBlur, Size(5, 5), 0);
+    namedWindow("blurred strawberries", WINDOW_NORMAL);
+    imshow("blurred strawberries", img_strawberry1);
 
     /// Click click click
     std::cerr << "Click on strawberries:\n";
-    setMouseCallback("strawberry1", onMouseStrawberries, 0 );
+    setMouseCallback("blurred strawberries", onMouseStrawberries, 0 );
     waitKey(0);
 
     std::cerr << "Click on background:\n";
-    setMouseCallback("strawberry1", onMouseBackground, 0 );
+    setMouseCallback("blurred strawberries", onMouseBackground, 0 );
     waitKey(0);
 
-    /// Training
+    /// Descriptors
     Mat img_hsv;
     cvtColor(img_strawberry1, img_hsv, COLOR_BGR2HSV);
 
@@ -80,7 +83,7 @@ int main( int argc, char** argv )
     Mat trainingDataForeground(strawberryPixels.size(), 3, CV_32FC1);
     Mat labels_fg = Mat::ones(strawberryPixels.size(), 1, CV_32FC1);
 
-    for(int i=0; i<strawberryPixels.size(); i++)
+    for(size_t i=0; i<strawberryPixels.size(); i++)
     {
         Vec3b descriptor = img_hsv.at<Vec3b>(strawberryPixels[i].y, strawberryPixels[i].x);
         trainingDataForeground.at<float>(i,0) = descriptor[0];
@@ -92,7 +95,7 @@ int main( int argc, char** argv )
     Mat trainingDataBackground(backgroundPixels.size(), 3, CV_32FC1);
     Mat labels_bg = Mat::zeros(backgroundPixels.size(), 1, CV_32FC1);
 
-    for(int i=0; i<strawberryPixels.size(); i++)
+    for(size_t i=0; i<strawberryPixels.size(); i++)
     {
         Vec3b descriptor = img_hsv.at<Vec3b>(strawberryPixels[i].y, strawberryPixels[i].x);
         trainingDataBackground.at<float>(i,0) = descriptor[0];
@@ -101,25 +104,92 @@ int main( int argc, char** argv )
     }
 
     ///group foreground and background
+    Mat trainingData;
+    Mat labels;
+
+    vconcat(trainingDataForeground, trainingDataBackground, trainingData);
+    vconcat(labels_fg, labels_bg, labels);
+
+    cout << trainingData.size() << endl;
+    cout << labels.size() << endl;
 
 
+    ///Training
+    cerr << "Training a 1 Nearest Neighbour Classifier ... " << endl;
+    Ptr<KNearest> kNN = KNearest::create();
+    Ptr<TrainData> trainingDataKNN = TrainData::create(trainingData, ROW_SAMPLE, labels);
+    kNN->setIsClassifier(true);
+    kNN->setAlgorithmType(KNearest::BRUTE_FORCE);
+    kNN->setDefaultK(3);
+    kNN->train(trainingDataKNN);
 
+    cerr << "Training a Normal Bayes Classifier ... " << endl;
+    Ptr<NormalBayesClassifier> normalBayes = NormalBayesClassifier::create();
+    normalBayes->train(trainingData, ROW_SAMPLE, labels);
 
-    /// nearest neighbour classifier
-    //Ptr<Knearest> kNN = KNearest::create();
-    //Ptr<TrainData> trainingDataKNN = TrainData::create
+    cerr << "Training a Support Vector Machine Classifier ... " << endl;
+    Ptr<SVM> svm = SVM::create();
+    svm->setType(SVM::C_SVC);
+    svm->setKernel(SVM::LINEAR);
+    svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 100, 1e-6));
+    svm->train(trainingData, ROW_SAMPLE, labels);
 
+    cerr << "Running the classifier on the input image and creating  masks" << endl;
+    Mat labels_kNN, labels_normalBayes, labels_SVM;
+    Mat mask_kNN = Mat::zeros(img_strawberry1.rows, img_strawberry1.cols, CV_8UC1);
+    Mat mask_normalBayes = Mat::zeros(img_strawberry1.rows, img_strawberry1.cols, CV_8UC1);
+    Mat mask_SVM = Mat::zeros(img_strawberry1.rows, img_strawberry1.cols, CV_8UC1);
 
-    //ORB_KeypointsDetection(img_templ.clone(), img_objects.clone());
-    //BRISK_KeypointsDetection(img_templ.clone(), img_objects.clone());
-    //AKAZE_KeypointsDetection(img_templ.clone(), img_objects.clone());
+    for(int i=0; i<img_strawberry1.rows; i++)
+    {
+        for(int j=0; j<img_strawberry1.cols; j++)
+        {
+            Vec3b pixelvalue = img_hsv.at<Vec3b>(i,j);
+            Mat data_test(1,3,CV_32FC1);
+            data_test.at<float>(0,0) = pixelvalue[0];
+            data_test.at<float>(0,1) = pixelvalue[1];
+            data_test.at<float>(0,2) = pixelvalue[2];
+
+            kNN->findNearest(data_test, kNN->getDefaultK(), labels_kNN);
+            normalBayes->predict(data_test, labels_normalBayes);
+            svm->predict(data_test, labels_SVM);
+
+            mask_kNN.at<uchar>(i,j) = labels_kNN.at<float>(0,0);
+            mask_normalBayes.at<uchar>(i,j) = labels_normalBayes.at<int>(0,0);
+            mask_SVM.at<uchar>(i,j) = labels_SVM.at<float>(0,0);
+        }
+    }
+
+    /// Visualize masks
+    namedWindow("Segmentation KNearest", WINDOW_NORMAL);
+    imshow("Segmentation KNearest", mask_kNN*255);
+    namedWindow("Segmentation Normal Bayes", WINDOW_NORMAL);
+    imshow("Segmentation Normal Bayes", mask_normalBayes*255);
+    namedWindow("Segmentation Support Vector Machine", WINDOW_NORMAL);
+    imshow("Segmentation Support Vector Machine", mask_SVM*255);
+    waitKey(0);
+
+    /// Visualize resulting foreground pixels
+    Mat result_kNN, result_normalBayes, result_SVM;
+
+    bitwise_and(img_strawberry1, img_strawberry1, result_kNN, mask_kNN);
+    bitwise_and(img_strawberry1, img_strawberry1, result_normalBayes, mask_normalBayes);
+    bitwise_and(img_strawberry1, img_strawberry1, result_SVM, mask_SVM);
+
+    namedWindow("KNearest", WINDOW_NORMAL);
+    imshow("KNearest", result_kNN);
+    waitKey(0);
+    namedWindow("Normal Bayes", WINDOW_NORMAL);
+    imshow("Normal Bayes", result_normalBayes);
+    waitKey(0);
+    namedWindow("Support Vector Machine", WINDOW_NORMAL);
+    imshow("Support Vector Machine", result_SVM);
+    waitKey(0);
 
 }
 
-
 static void onMouseStrawberries( int event, int x, int y, int, void* /*param*/)
 {
-
     if(event == EVENT_LBUTTONDOWN)
     {
         strawberryPixels.push_back(Point(x,y));
@@ -139,7 +209,11 @@ static void onMouseStrawberries( int event, int x, int y, int, void* /*param*/)
     }
     if(event == EVENT_MBUTTONDOWN)
     {
-        return;
+        std::cout << "List of strawberry points\n";
+        for (size_t i=0; i<strawberryPixels.size(); i++)
+        {
+            std::cout << strawberryPixels[i] << "\n";
+        }
     }
 }
 
@@ -166,165 +240,10 @@ static void onMouseBackground( int event, int x, int y, int, void* )
     }
     if(event == EVENT_MBUTTONDOWN)
     {
-        return;
+        std::cout << "List of background points\n";
+        for (size_t i=0; i<backgroundPixels.size(); i++)
+        {
+            std::cout << backgroundPixels[i] << "\n";
+        }
     }
-}
-
-
-void ORB_KeypointsDetection(Mat tmpl, Mat img)
-{
-    // detect keypoints
-    //int nfeatures = 500;
-
-    //ORB
-    Ptr<ORB> detector = ORB::create();
-    std::vector<KeyPoint> keypoints_tmpl;
-    std::vector<KeyPoint> keypoints_img;
-    Mat descriptor_tmpl;
-    Mat descriptor_img;
-
-    detector->detectAndCompute(tmpl, Mat(), keypoints_tmpl, descriptor_tmpl);
-    detector->detectAndCompute(img, Mat(), keypoints_img, descriptor_img);
-
-    // draw keypoints
-    Mat img_keypoints;
-    Mat tmpl_keypoints;
-
-    drawKeypoints(tmpl.clone(), keypoints_tmpl, tmpl_keypoints);
-    drawKeypoints(img.clone(), keypoints_img, img_keypoints);
-
-    // show detected keypoints
-    namedWindow("ORB detected keypoints template", WINDOW_NORMAL);
-    imshow("ORB detected keypoints template", tmpl_keypoints);
-    waitKey(0);
-    namedWindow("ORB detected keypoints image", WINDOW_NORMAL);
-    imshow("ORB detected keypoints image", img_keypoints);
-    waitKey(0);
-
-    KeypointMatching("ORB", tmpl, img, keypoints_tmpl, keypoints_img, descriptor_tmpl, descriptor_img);
-}
-
-void BRISK_KeypointsDetection(Mat tmpl, Mat img)
-{
-    // detect keypoints
-    //int nfeatures = 500;
-
-    //BRISK
-    Ptr<BRISK> detector = BRISK::create();
-    std::vector<KeyPoint> keypoints_tmpl;
-    std::vector<KeyPoint> keypoints_img;
-    Mat descriptor_tmpl;
-    Mat descriptor_img;
-
-    detector->detectAndCompute(tmpl, Mat(), keypoints_tmpl, descriptor_tmpl);
-    detector->detectAndCompute(img, Mat(), keypoints_img, descriptor_img);
-
-    // draw keypoints
-    Mat img_keypoints;
-    Mat tmpl_keypoints;
-
-    drawKeypoints(tmpl.clone(), keypoints_tmpl, tmpl_keypoints);
-    drawKeypoints(img.clone(), keypoints_img, img_keypoints);
-
-    // show detected keypoints
-    namedWindow("BRISK detected keypoints template", WINDOW_NORMAL);
-    imshow("BRISK detected keypoints template", tmpl_keypoints);
-    waitKey(0);
-    namedWindow("BRISK detected keypoints image", WINDOW_NORMAL);
-    imshow("BRISK detected keypoints image", img_keypoints);
-    waitKey(0);
-
-    KeypointMatching("BRISK", tmpl, img, keypoints_tmpl, keypoints_img, descriptor_tmpl, descriptor_img);
-}
-
-void AKAZE_KeypointsDetection(Mat tmpl, Mat img)
-{
-    // detect keypoints
-    //int nfeatures = 500;
-
-    //AKAZE
-    Ptr<AKAZE> detector = AKAZE::create();
-    std::vector<KeyPoint> keypoints_tmpl;
-    std::vector<KeyPoint> keypoints_img;
-    Mat descriptor_tmpl;
-    Mat descriptor_img;
-
-    detector->detectAndCompute(tmpl, Mat(), keypoints_tmpl, descriptor_tmpl);
-    detector->detectAndCompute(img, Mat(), keypoints_img, descriptor_img);
-
-    // draw keypoints
-    Mat img_keypoints;
-    Mat tmpl_keypoints;
-
-    drawKeypoints(tmpl.clone(), keypoints_tmpl, tmpl_keypoints);
-    drawKeypoints(img.clone(), keypoints_img, img_keypoints);
-
-    // show detected keypoints
-    namedWindow("AKAZE detected keypoints template", WINDOW_NORMAL);
-    imshow("AKAZE detected keypoints template", tmpl_keypoints);
-    waitKey(0);
-    namedWindow("AKAZE detected keypoints image", WINDOW_NORMAL);
-    imshow("AKAZE detected keypoints image", img_keypoints);
-    waitKey(0);
-
-    KeypointMatching("AKAZE", tmpl, img, keypoints_tmpl, keypoints_img, descriptor_tmpl, descriptor_img);
-}
-
-void KeypointMatching(string name, Mat tmpl, Mat img, std::vector<KeyPoint> keypoints_tmpl, std::vector<KeyPoint> keypoints_img, Mat descriptor_tmpl, Mat descriptor_img)
-{
-    float GOOD_MATCH_PERCENT = 0.15f;
-
-    // find matches
-    BFMatcher matcher(NORM_L2);
-    std::vector<DMatch> matches;
-    matcher.match(descriptor_tmpl, descriptor_img, matches);
-
-    // Sort matches by score
-    std::sort(matches.begin(), matches.end());
-
-    // Remove not so good matches
-    const int numGoodMatches = matches.size() * GOOD_MATCH_PERCENT;
-    matches.erase(matches.begin()+numGoodMatches, matches.end());
-
-    // draw matches
-    Mat img_matches;
-    drawMatches(tmpl, keypoints_tmpl, img, keypoints_img, matches, img_matches);
-
-    namedWindow(name, WINDOW_NORMAL);
-    imshow(name, img_matches);
-    waitKey(0);
-
-    // Extract location of good matches
-    std::vector<Point2f> points_tmp, points_img;
-
-    for( size_t i = 0; i < matches.size(); i++ )
-    {
-      points_tmp.push_back( keypoints_tmpl[ matches[i].queryIdx ].pt );
-      points_img.push_back( keypoints_img[ matches[i].trainIdx ].pt );
-    }
-
-    Mat homography = findHomography( points_tmp, points_img, RANSAC );
-
-    std::vector<Point2f> obj_corners(4);
-    obj_corners[0] = cvPoint(0,0);
-    obj_corners[1] = CvPoint(tmpl.cols);
-    obj_corners[2] = cvPoint(tmpl.cols, tmpl.rows);
-    obj_corners[3] = cvPoint(0, tmpl.rows);
-    std::vector<Point2f> scene_corners(4);
-    perspectiveTransform(obj_corners, scene_corners, homography);
-
-    // translate corners over tmpl width
-    scene_corners[0].x += tmpl.cols;
-    scene_corners[1].x += tmpl.cols;
-    scene_corners[2].x += tmpl.cols;
-    scene_corners[3].x += tmpl.cols;
-
-    line(img_matches, scene_corners[0], scene_corners[1],Scalar(0,255,0), 3);
-    line(img_matches, scene_corners[1], scene_corners[2],Scalar(0,255,0), 3);
-    line(img_matches, scene_corners[2], scene_corners[3],Scalar(0,255,0), 3);
-    line(img_matches, scene_corners[3], scene_corners[0],Scalar(0,255,0), 3);
-
-    namedWindow("tadaa "+name, WINDOW_NORMAL);
-    imshow("tadaa "+name, img_matches);
-    waitKey(0);
 }
