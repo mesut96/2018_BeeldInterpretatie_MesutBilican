cc59bbb4b99954429c8ff29882328c243587f9f4
mesut96 <mesut.bilican@hotmail.com>
2018-12-05 20:04:14 +0100

    fixed bug

--- Sessie_5/main.cpp
+++ Sessie_5/main.cpp
@@ -6,7 +6,6 @@
 #include "opencv2/highgui.hpp"
 #include <opencv2/features2d/features2d.hpp>
 
-
 using namespace std;
 using namespace cv;
 using namespace ml;
@@ -18,70 +17,66 @@ static void onMouseBackground( int event, int x, int y, int, void* );
 /// Globals
 vector<Point> strawberryPixels;
 vector<Point> backgroundPixels;
+Mat img_strawberry;
 
 int main( int argc, char** argv )
 {
     /// Adding a little help option and command line parser input
     CommandLineParser parser(argc, argv,
         "{ help h usage ? || show this message }"
-        "{ image_strawberry1 s1  || (required) path to image }"
-        "{ image_strawberry2 s2 || (required) path to image }"
+        "{ image_strawberry is  || (required) path to image }"
     );
     /// If help is entered
     if (parser.has("help"))
     {
         parser.printMessage();
-        cerr << "use parameters: --image_strawberry1=strawberry1.tif --image_strawberry2=strawberry2.tif";
+        cerr << "use parameters: --image_strawberry=strawberry1.tif";
         return 0;
     }
 
     /// Collect data from arguments
-    string imagepath_strawberry1(parser.get<string>("image_strawberry1"));
-    string imagepath_strawberry2(parser.get<string>("image_strawberry2"));
-    if (imagepath_strawberry1.empty() || imagepath_strawberry2.empty())
+    string imagepath_strawberry(parser.get<string>("image_strawberry"));
+
+    if (imagepath_strawberry.empty())
     {
         cerr << "image not found\n";
         parser.printMessage();
         return -1;
     }
 
-    /// Read and show strawberry 1
-    Mat img_strawberry1;
-    img_strawberry1 = imread(imagepath_strawberry1);
-    namedWindow("strawberry1", WINDOW_NORMAL);
-    imshow("strawberry1", img_strawberry1);
-    waitKey(0);
-
-    /// Read and show strawberry 2
-    //Mat img_strawberry2;
-    //img_strawberry2 = imread(imagepath_strawberry2);
-    //namedWindow("strawberry2", WINDOW_NORMAL);
-    //imshow("strawberry2", img_strawberry2);
+    /// Read and show strawberries
+    img_strawberry = imread(imagepath_strawberry);
+    //namedWindow("strawberries", WINDOW_NORMAL);
+    //imshow("strawberries", img_strawberry);
     //waitKey(0);
+
 
     /// Gaussian blur
     Mat gaussBlur;
-
-    GaussianBlur(img_strawberry1, gaussBlur, Size(5, 5), 0);
+    GaussianBlur(img_strawberry, gaussBlur, Size(5, 5), 0);
     namedWindow("blurred strawberries", WINDOW_NORMAL);
-    imshow("blurred strawberries", img_strawberry1);
+    imshow("blurred strawberries", img_strawberry);
 
     /// Click click click
-    std::cerr << "Click on strawberries:\n";
+    cerr << "##############################################" << endl;
+    cerr << "          Click on strawberries:\n";
+    cerr << "##############################################" << endl;
     setMouseCallback("blurred strawberries", onMouseStrawberries, 0 );
     waitKey(0);
 
-    std::cerr << "Click on background:\n";
+    cerr << "##############################################" << endl;
+    cerr << "           Click on background:\n";
+    cerr << "##############################################" << endl;
     setMouseCallback("blurred strawberries", onMouseBackground, 0 );
     waitKey(0);
 
     /// Descriptors
     Mat img_hsv;
-    cvtColor(img_strawberry1, img_hsv, COLOR_BGR2HSV);
+    cvtColor(img_strawberry, img_hsv, COLOR_BGR2HSV);
 
     ///foreground training using HSV values as descriptor
     Mat trainingDataForeground(strawberryPixels.size(), 3, CV_32FC1);
-    Mat labels_fg = Mat::ones(strawberryPixels.size(), 1, CV_32FC1);
+    Mat labels_fg = Mat::ones(strawberryPixels.size(), 1, CV_32SC1); // Fixed by bug detective Dries en Simon
 
     for(size_t i=0; i<strawberryPixels.size(); i++)
     {
@@ -93,11 +88,11 @@ int main( int argc, char** argv )
 
     ///background training using HSV values as descriptor
     Mat trainingDataBackground(backgroundPixels.size(), 3, CV_32FC1);
-    Mat labels_bg = Mat::zeros(backgroundPixels.size(), 1, CV_32FC1);
+    Mat labels_bg = Mat::zeros(backgroundPixels.size(), 1, CV_32SC1); // Fixed by bug detective Dries en Simon
 
-    for(size_t i=0; i<strawberryPixels.size(); i++)
+    for(size_t i=0; i<backgroundPixels.size(); i++)
     {
-        Vec3b descriptor = img_hsv.at<Vec3b>(strawberryPixels[i].y, strawberryPixels[i].x);
+        Vec3b descriptor = img_hsv.at<Vec3b>(backgroundPixels[i].y, backgroundPixels[i].x);
         trainingDataBackground.at<float>(i,0) = descriptor[0];
         trainingDataBackground.at<float>(i,1) = descriptor[1];
         trainingDataBackground.at<float>(i,2) = descriptor[2];
@@ -108,14 +103,16 @@ int main( int argc, char** argv )
     Mat labels;
 
     vconcat(trainingDataForeground, trainingDataBackground, trainingData);
+    //cerr << trainingData << endl;
     vconcat(labels_fg, labels_bg, labels);
-
-    cout << trainingData.size() << endl;
-    cout << labels.size() << endl;
+    //cerr << labels << endl;
+    //cout << trainingData.size() << endl;
+    //cout << labels.size() << endl;
 
 
     ///Training
     cerr << "Training a 1 Nearest Neighbour Classifier ... " << endl;
+    cerr << "##############################################" << endl;
     Ptr<KNearest> kNN = KNearest::create();
     Ptr<TrainData> trainingDataKNN = TrainData::create(trainingData, ROW_SAMPLE, labels);
     kNN->setIsClassifier(true);
@@ -134,15 +131,15 @@ int main( int argc, char** argv )
     svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 100, 1e-6));
     svm->train(trainingData, ROW_SAMPLE, labels);
 
-    cerr << "Running the classifier on the input image and creating  masks" << endl;
+    cerr << "Running the classifier on the input image and creating  masks ..." << endl;
     Mat labels_kNN, labels_normalBayes, labels_SVM;
-    Mat mask_kNN = Mat::zeros(img_strawberry1.rows, img_strawberry1.cols, CV_8UC1);
-    Mat mask_normalBayes = Mat::zeros(img_strawberry1.rows, img_strawberry1.cols, CV_8UC1);
-    Mat mask_SVM = Mat::zeros(img_strawberry1.rows, img_strawberry1.cols, CV_8UC1);
+    Mat mask_kNN = Mat::zeros(img_strawberry.rows, img_strawberry.cols, CV_8UC1);
+    Mat mask_normalBayes = Mat::zeros(img_strawberry.rows, img_strawberry.cols, CV_8UC1);
+    Mat mask_SVM = Mat::zeros(img_strawberry.rows, img_strawberry.cols, CV_8UC1);
 
-    for(int i=0; i<img_strawberry1.rows; i++)
+    for(int i=0; i<img_strawberry.rows; i++)
     {
-        for(int j=0; j<img_strawberry1.cols; j++)
+        for(int j=0; j<img_strawberry.cols; j++)
         {
             Vec3b pixelvalue = img_hsv.at<Vec3b>(i,j);
             Mat data_test(1,3,CV_32FC1);
@@ -169,12 +166,21 @@ int main( int argc, char** argv )
     imshow("Segmentation Support Vector Machine", mask_SVM*255);
     waitKey(0);
 
+    erode(mask_kNN, mask_kNN, Mat(), Point(-1,-1), 1);
+    dilate(mask_kNN, mask_kNN, Mat(), Point(-1,-1), 2);
+
+    erode(mask_normalBayes, mask_normalBayes, Mat(), Point(-1,-1), 1);
+    dilate(mask_normalBayes, mask_normalBayes, Mat(), Point(-1,-1), 2);
+
+    erode(mask_SVM, mask_SVM, Mat(), Point(-1,-1), 1);
+    dilate(mask_SVM, mask_SVM, Mat(), Point(-1,-1), 2);
+
     /// Visualize resulting foreground pixels
     Mat result_kNN, result_normalBayes, result_SVM;
 
-    bitwise_and(img_strawberry1, img_strawberry1, result_kNN, mask_kNN);
-    bitwise_and(img_strawberry1, img_strawberry1, result_normalBayes, mask_normalBayes);
-    bitwise_and(img_strawberry1, img_strawberry1, result_SVM, mask_SVM);
+    bitwise_and(img_strawberry, img_strawberry, result_kNN, mask_kNN);
+    bitwise_and(img_strawberry, img_strawberry, result_normalBayes, mask_normalBayes);
+    bitwise_and(img_strawberry, img_strawberry, result_SVM, mask_SVM);
 
     namedWindow("KNearest", WINDOW_NORMAL);
     imshow("KNearest", result_kNN);
@@ -185,6 +191,8 @@ int main( int argc, char** argv )
     namedWindow("Support Vector Machine", WINDOW_NORMAL);
     imshow("Support Vector Machine", result_SVM);
     waitKey(0);
+
+    return 0;
 
 }
 
@@ -210,9 +218,12 @@ static void onMouseStrawberries( int event, int x, int y, int, void* /*param*/)
     if(event == EVENT_MBUTTONDOWN)
     {
         std::cout << "List of strawberry points\n";
+        Mat temp = img_strawberry.clone();
         for (size_t i=0; i<strawberryPixels.size(); i++)
         {
-            std::cout << strawberryPixels[i] << "\n";
+            std::cout << strawberryPixels[i] << endl;
+            circle(temp,strawberryPixels[i],3, Scalar(0,0,255),-1);
+            imshow("blurred strawberries", temp);
         }
     }
 }
@@ -224,7 +235,7 @@ static void onMouseBackground( int event, int x, int y, int, void* )
     if(event == EVENT_LBUTTONDOWN)
     {
         backgroundPixels.push_back(Point(x,y));
-        std::cerr << Point(x,y) << "\n";
+        std::cerr << Point(x,y) << endl;
     }
     if(event == EVENT_RBUTTONDOWN)
     {
@@ -241,9 +252,12 @@ static void onMouseBackground( int event, int x, int y, int, void* )
     if(event == EVENT_MBUTTONDOWN)
     {
         std::cout << "List of background points\n";
+        Mat temp = img_strawberry.clone();
         for (size_t i=0; i<backgroundPixels.size(); i++)
         {
-            std::cout << backgroundPixels[i] << "\n";
+            std::cout << backgroundPixels[i] << endl;
+            circle(temp,backgroundPixels[i],3, Scalar(255,0,0),-1);
+            imshow("blurred strawberries", temp);
         }
     }
 }
